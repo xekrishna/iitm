b12ed8791d44922339aba6540fea332f2c407bda
xekrishna <octpahadi@gmail.com>
2024-06-12 16:42:56 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function joinDC () {
+    location.href = "https://discord.com/invite/iitm"
+}
+
+function learnMore () {
+    location.href = "https://study.iitm.ac.in/ds/"
+}
+
+function aboutUs () {
+    location.href = "https://www.iitm.ac.in/the-institute/about-iit-madras/overview"
+}
+
+function contactUs () {
+    location.href = "https://www.iitm.ac.in/the-institute/about-iit-madras/contact-us"
+}
